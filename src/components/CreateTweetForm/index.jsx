92a27abf9d6c0ae38b0868bdd31ccd8265a5830a
Frantz Chery <frantzchery@hotmail.com>
2024-06-06 15:08:04 -0400

    update commit 2

--- src/components/CreateTweetForm/index.jsx
+++ src/components/CreateTweetForm/index.jsx
@@ -1,33 +1,42 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
-import styles from "./CreateTweetForm.module.css";
-import { Button, Form } from "react-bootstrap";
+import { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import styles from './CreateTweetForm.module.css';
+import { Button, Form } from 'react-bootstrap';
 
 function CreateTweetForm({ addTweet }) {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('');
+  const contentInputRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (contentInputRef.current.value === '') {
+      contentInputRef.current.focus();
+      // alert('Please complete form');
+      return;
+    }
+
     addTweet(content);
-    setContent("");
+    setContent('');
   };
 
   return (
     <Form onSubmit={handleSubmit} className={styles.container}>
-      <Form.Label htmlFor="content">
+      <Form.Label htmlFor='content'>
         <h4>whats in you mind?</h4>
       </Form.Label>
 
       <Form.Control
-        className="mb-4"
-        type="text"
-        name="content"
-        id="content"
+        className='mb-4'
+        type='text'
+        name='content'
+        id='content'
         value={content}
         onChange={(e) => setContent(e.target.value)}
+        ref={contentInputRef}
       />
 
-      <Button type="submit">Tweet</Button>
+      <Button type='submit'>Tweet</Button>
     </Form>
   );
 }
--- src/components/UpdateTweetForm/index.jsx
+++ src/components/UpdateTweetForm/index.jsx
@@ -1,23 +1,45 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Modal, Form, Button, ModalBody } from 'react-bootstrap';
+import { IoMdClose } from 'react-icons/io';
 
 function UpdateTweetForm({ tweet, setShowModal, updateTweet }) {
   const [newTweetContent, setNewTweetContent] = useState(tweet.content);
 
-
   const handleSubmit = (e) => {
-    e.preventDefault()
-    updateTweet(tweet.id, newTweetContent)
-    setShowModal(false)
-  }
+    e.preventDefault();
+    updateTweet(tweet.id, newTweetContent);
+    setShowModal(false);
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={newTweetContent} 
-      onChange={(e) => setNewTweetContent(e.target.value) }/>
+    <div
+    //   className='modal show'
+    //   style={{
+    //     display: 'flex',
+    //     position: 'absolute',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //   }}
+    >
+      <Modal.Dialog style={{minWidth: '500px'}}>
+        <IoMdClose className='m-2' onClick={() => setShowModal(false)} />
 
-      <input type="submit" value="save" />
-    </form>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Control
+              type='text'
+              value={newTweetContent}
+              onChange={(e) => setNewTweetContent(e.target.value)}
+            />
+
+            <Button type='submit' variant='success' className='mt-2'>
+              Save
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal.Dialog>
+    </div>
   );
 }
 
